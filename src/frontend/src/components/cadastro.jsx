42bb02dfb748914dff0934a2fd6c01da250eedac
Seu Nome <seu.email@exemplo.com>
2024-06-05 22:34:09 -0300

    mensagem de erro no login do front

--- src/frontend/src/components/cadastro.jsx
+++ src/frontend/src/components/cadastro.jsx
@@ -1,23 +1,34 @@
 import './login.css'
 import Button from './button.jsx'
 import { userRegister } from '../services/userServices.js';
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Login(props) {
+    const [message, setMessage] = useState(null);
+    const [isSuccess, setIsSuccess] = useState(false);
     const handleCriar = async (event) => {
         event.preventDefault();
         console.log(event.target.name.value)
-        await userRegister(
+        const result = await userRegister(
             event.target.name.value,
             event.target.email.value,
             event.target.password.value,
             event.target.confirmPassword.value
         )
+        setMessage(result.message);
+        setIsSuccess(result.success);
+        if (result.success) { window.location.href = '/login' }
+
         //  props.onCreate()
     }
 
     return (
         <>
             <form method='POST' className="login" onSubmit={handleCriar}>
+                {isSuccess ? <p>Registro bem-sucedido! </p> : <p> {message} </p>}
                 <input name="name" type="text" placeholder='Nome' />
                 <input name="email" type="text" placeholder='E-mail' />
                 <input name="password" type="text" placeholder='Senha' />
--- src/frontend/src/services/userServices.js
+++ src/frontend/src/services/userServices.js
@@ -1,19 +1,34 @@
-import axios from "axios"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export function userRegister(name, email, password, confirmPassword) {
-    return axios.post("http://localhost:3333/users/register", {
-        name: name,
-        email: email,
-        password: password,
-        confirmPassword: confirmPassword
-    }, {
-        headers: { 'Content-Type': 'application/json' }
-    })
+export async function userRegister(name, email, password, confirmPassword) {
+    try {
+        const res = await axios.post("http://localhost:3333/users/register",
+            {
+                name: name,
+                email: email,
+                password: password,
+                confirmPassword: confirmPassword
+            }, { headers: { 'Content-Type': 'application/json' } });
+
+        if (res.status === 201 || res.status === 204 || res.status === 200) {
+            return { success: true, message: "Registro bem-sucedido!" };
+        }
+
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            return { success: false, message: "Usuário ou senha incorretos." };
+        } else {
+            console.error("Erro ao fazer registro:", error);
+            return { success: false, message: "Não foi possível realizar o cadastro. Tente novamente mais tarde." };
+        }
+    }
 }
 
+//
 export async function userlogin(email, password) {
     console.log("entrei")
-    const response = await axios.post("http://192.168.100.67:3333/users/login", {
+    const response = await axios.post("localhost:3333/users/login", {
         email: email,
         password: password,
     })
