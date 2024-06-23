5ddf1b9eb1ca117e4c80fa9cd26c6554473b26f7
elvesdias <112361834+elvesdias@users.noreply.github.com>
2024-06-23 19:01:58 -0300

    Add files via upload

--- src/mobile/gymgo/App.tsx
+++ src/mobile/gymgo/App.tsx
@@ -1,25 +1,29 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Loading } from './src/components/loading';
+
+import { AuthProvider } from 'src/context/authContext';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-  return (
-    <NativeBaseProvider theme= {THEME}>
-       <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-        />      
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </NativeBaseProvider>
-  );
+    return (
+        <AuthProvider>
+            <NativeBaseProvider theme={THEME}>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor="transparent"
+                    translucent
+                />
+                {fontsLoaded ? <Routes /> : <Loading />}
+            </NativeBaseProvider>
+        </AuthProvider>
+    );
 }
 
 
